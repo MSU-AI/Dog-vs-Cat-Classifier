ec714a7cd0eb5b4d7519810e0d9a5082c5719828
Gabriel Sotelo <gabrielsotelojusto@hotmail.com>
2021-09-30 17:42:12 -0400

    add

--- main.py
+++ main.py
@@ -4,81 +4,77 @@ import matplotlib.pyplot as plt
 
 from keras_preprocessing.image import ImageDataGenerator, load_img, img_to_array
 
-def plot_result(history):
-    acc = history.history['accuracy']
-    val_acc = history.history['val_accuracy']
-    epochs = range(len(acc))
-
-    plt.plot(epochs, acc, 'b', label='Training accuracy')
-    plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
-    plt.title('Training and validation accuracy')
-    plt.legend()
-    plt.figure()
-
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    plt.plot(epochs, loss, 'b', label='Training Loss')
-    plt.plot(epochs, val_loss, 'r', label='Validation Loss')
-    plt.title('Training and validation loss')
-    plt.legend()
-
-    plt.show()
-
+# Initialize an object that automatically formats the image data
+ImageGenerator1 = ImageDataGenerator(rescale=1. / 255)
+ImageGenerator2 = ImageDataGenerator(rescale=1. / 255)
 
 def train_model():
-    train_gen = ImageDataGenerator(
-        rescale=1. / 255,
-    )
-
-    train_iterator = train_gen.flow_from_directory('train',
+    # Store all the images in train folder to train_images object,
+    # using ImageGenerator1 as constructor.
+    # Specify the size you want to resize all images to
+    train_images = ImageGenerator1.flow_from_directory('train',
                                                    target_size=(150, 150),
-                                                   batch_size=20,
                                                    class_mode='binary')
-
-    validation_gen = ImageDataGenerator(rescale=1. / 255.0)
-    validation_iterator = validation_gen.flow_from_directory('validate',
+    
+    # Store all the images in validation folder to validation_images object,
+    # using ImageGenerator2 as constructor.
+    # Specify the size you want to resize all images to
+    validation_images = ImageGenerator2.flow_from_directory('validate',
                                                              target_size=(150, 150),
-                                                             batch_size=10,
                                                              class_mode='binary')
 
+    # Start building your Neural Network
     model = keras.models.Sequential([
+        # Input layer and first convolutional layer of 16 filters
         keras.layers.Conv2D(16, (3, 3), activation="relu", input_shape=(150, 150, 3)),
         
+        # Apply pooling to your image
         keras.layers.MaxPool2D((2, 2)),
         
+        # Second convolutional layer of 32 filters
         keras.layers.Conv2D(32, (3, 3), activation="relu"),
         
+        # Apply pooling to your image
         keras.layers.MaxPool2D((2, 2)),
         
+        # Third convolutional later of 64 filters
         keras.layers.Conv2D(64, (3, 3), activation="relu"),
         
+        # Apply pooling to your image
         keras.layers.MaxPool2D((2, 2)),
         
+        # Flatten your image
         keras.layers.Flatten(),
         
+        # Hidden layer of 512 neurons
         keras.layers.Dense(units=512, activation="relu"),
         
+        # Output layer of 1 neuron (goes from 0 to 1 where 0 is cat and 1 is dog)
         keras.layers.Dense(1, activation="sigmoid")
     ])
-
-    model.compile(optimizer=tf.optimizers.Adam(),
-                  loss=tf.keras.losses.binary_crossentropy,
+    
+    # Compile your neural network
+    model.compile(optimizer="adam",
+                  loss="binary_crossentropy",
                   metrics=['accuracy'])
-
-    history = model.fit(train_iterator,
-                        validation_data=validation_iterator,
-                        steps_per_epoch=1000,
-                        epochs=50,
-                        validation_steps=500)
+    
+    # Fit your model with the training images, and validate with the validation images
+    model.fit(train_images,
+            validation_data=validation_images,
+            steps_per_epoch=1000,
+            epochs=50,
+            validation_steps=500)
     
     model.save('model.h5')
 
-    return history
+    return model
 
 
 def load_and_predict(filename):
-    model = keras.models.load_model('model.h5')
-    
+    try:
+        model = keras.models.load_model('model.h5')
+    except:
+        model = train_model()
     image = load_img(f'test/{filename}', target_size=(150,150))
     image = img_to_array(image)
     image /= 255
@@ -87,7 +83,8 @@ def load_and_predict(filename):
     prediction = model.predict(image)
     
     return prediction
-    
+
+
 def main():
     while True:
         filename = input('Input the name of the file you want to classify ("q" to quit): ')
